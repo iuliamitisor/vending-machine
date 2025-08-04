b4278393263267e3e77bb2745eaa35d44c44b3a6
iuliamitisor <iulia.mitisor@gmail.com>
2025-08-04 14:57:33 +0300

    Modify ProductService to register each sale (#8)

--- src/main/java/org/vendingmachine/repository/SaleRepository.java
+++ src/main/java/org/vendingmachine/repository/SaleRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 
 @Repository
 public interface SaleRepository extends JpaRepository<Sale, Long> {
-
+    List<Sale> findByTimestampBetween(LocalDateTime start, LocalDateTime end);
 }
 
--- src/main/java/org/vendingmachine/service/ProductService.java
+++ src/main/java/org/vendingmachine/service/ProductService.java
@@ -3,17 +3,22 @@ package org.vendingmachine.service;
 import org.vendingmachine.exception.*;
 import org.vendingmachine.model.Product;
 import org.springframework.stereotype.Service;
+import org.vendingmachine.model.Sale;
 import org.vendingmachine.repository.ProductRepository;
+import org.vendingmachine.repository.SaleRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 public class ProductService {
 
     private final ProductRepository productRepository;
+    private final SaleRepository saleRepository;
 
-    public ProductService(ProductRepository productRepository) {
+    public ProductService(ProductRepository productRepository, SaleRepository saleRepository) {
         this.productRepository = productRepository;
+        this.saleRepository = saleRepository;
     }
 
     public List<Product> getAllProducts() {
@@ -42,13 +47,14 @@ public class ProductService {
         if (cashAmount >= requiredAmount) {
             boughtProduct.decrementQuantity(1);
             productRepository.save(boughtProduct);
+            registerSale(columnId, requiredAmount, "cash", cashAmount - requiredAmount);
         }
 
         if (cashAmount < requiredAmount) {
             throw new PaymentValidationException("Insufficient cash amount.", columnId);
         }
         if (cashAmount > requiredAmount) {
-            throw new ExcessCashException("Excess cash amount.", columnId, (float)(cashAmount - requiredAmount));
+            throw new ExcessCashException("Excess cash amount.", columnId, (float) (cashAmount - requiredAmount));
         }
     }
 
@@ -56,5 +62,13 @@ public class ProductService {
         // Assume card payment always successful
         boughtProduct.decrementQuantity(1);
         productRepository.save(boughtProduct);
+        registerSale(boughtProduct.getColumnId(), boughtProduct.getPrice(), "card", 0.0f);
+    }
+
+    public void registerSale(int columnId, float price, String method, float change) {
+        Sale sale = new Sale(columnId, price, method, change, LocalDateTime.now());
+        saleRepository.save(sale);
     }
 }
+
+
